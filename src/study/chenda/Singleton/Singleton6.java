a1d48360e0a62b525b8a04062a1202b0811cad57
zxcv1129678576 <1129678576@qq.com>
2018-03-11 16:57:19 +0800

    单例模式双重验证

--- src/study/chenda/Singleton/Singleton6.java
+++ src/study/chenda/Singleton/Singleton6.java
@@ -1,6 +1,20 @@
 package study.chenda.Singleton;
 
 public class Singleton6 {
+	private volatile static Singleton6 instance;
 	
+	private Singleton6(){
+		
+	}
+	
+	public static Singleton6 getinstance(){
+		if(instance==null){
+			synchronized(Singleton6.class){
+				instance=new Singleton6();
+			}
+		}
+		
+		return instance;
+	}
 	
 }
